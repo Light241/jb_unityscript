db46b774e288b89d00a10ad107e3f9c90e14d837
S.Panfilov <se-panfilov@ya.ru>
2015-02-12 15:03:52 +0800

    Defined a UnityScriptFile

---
+++ src/com/panfilov/unityscript/psi/UnityScriptFile.java
@@ -0,0 +1,33 @@
+package com.panfilov.unityscript.psi;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import com.panfilov.unityscript.UnityScriptFileType;
+import com.panfilov.unityscript.UnityScriptLanguage;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class UnityScriptFile extends PsiFileBase {
+
+    public UnityScriptFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, UnityScriptLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return UnityScriptFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "UnityScript File";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+}
