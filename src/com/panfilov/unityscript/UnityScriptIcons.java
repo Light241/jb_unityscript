4f75f38060c43b6904396035976b6cc6bdfa58c8
S.Panfilov <se-panfilov@ya.ru>
2015-02-12 14:31:12 +0800

    fixes gor icon

--- src/com/panfilov/unityscript/UnityScriptIcons.java
+++ src/com/panfilov/unityscript/UnityScriptIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class UnityScriptIcons {
-    public static final Icon FILE = IconLoader.getIcon("com/panfilov/unityscript/img/unityscript_icn.png");
+    public static final Icon FILE = IconLoader.getIcon("/com/panfilov/unityscript/img/unityscript_icn.png");
 }
